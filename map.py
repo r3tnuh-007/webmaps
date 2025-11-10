de7f9323a641bda2d2331cd8b45c771f2a895236
r3tnuh-007 <anterofranciso@gmail.com>
2025-11-10 19:29:07 +0100

    Web map finished
    created the markers, circleMarkers, range of population feature and a layer control feature

--- map.py
+++ map.py
@@ -5,8 +5,14 @@ map = Map(location=[32.5805, -99.6750], zoom_start=7, tiles="OpenStreetMap")
 #Creating a feature group
 fg = FeatureGroup(name="My Map")
 
-fg.add_child(Marker(location=[32.5805, -99.6750], popup="Center of Texas", icon=Icon(color='red')))
+fg.add_child(CircleMarker(location=[32.5805, -99.6750], popup="Center of Texas", icon=Icon(color='red', radius=6)))
+
+fg.add_child(GeoJson(data=open("world.json", 'r', encoding='utf-8-sig').read(),
+style_function=lambda x:  {"fillColor":"green" if x['properties']['POP2005'] < 10000000
+else "orange" if 10000000 <= x['properties']['POP2005'] < 20000000
+else "red"}))
 
 map.add_child(fg)
+map.add_child(LayerControl())
 
 map.save("map1.html")
